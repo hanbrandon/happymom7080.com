1837b1eadcc002d9407a6662bd7dee678019c46f
Sang Hyun Han <hanbrandon@hotmail.com>
2026-05-07 14:20:56 -0700

    fix: rename proxy.ts to middleware.ts and update matcher to fix static asset 404s

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,5 +9,5 @@ export const config = {
   // - /_next (Next.js internals)
   // - /_static (inside /public)
   // - all root files inside /public (e.g. /favicon.ico)
-  matcher: ['/((?!api|_next|_static|_vercel|[\\w-]+\\.\\w+).*)']
+  matcher: ['/((?!api|_next|_static|_vercel|.*\\..*).*)']
 };
